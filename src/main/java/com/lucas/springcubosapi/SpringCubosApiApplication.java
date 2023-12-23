6d3066cdac673172a714bf43069fb1a5a970bb6a
Lucas Morais <lucas14.morais@gmail.com>
2023-12-22 23:44:12 -0300

    chore: initialize project

---
+++ src/main/java/com/lucas/springcubosapi/SpringCubosApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.springcubosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCubosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCubosApiApplication.class, args);
+	}
+
+}
