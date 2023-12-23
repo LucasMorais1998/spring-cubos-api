ff6842d79ad4a62d9b450825041ff9a7c56244e5
Lucas Morais <lucas14.morais@gmail.com>
2023-12-23 15:38:39 -0300

    feat(client): add Client entity

---
+++ src/main/java/com/lucas/springcubosapi/entities/Client.java
@@ -0,0 +1,33 @@
+package com.lucas.springcubosapi.entities;
+
+import com.lucas.springcubosapi.dtos.ClientDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity(name = "clients")
+@Table(name = "clients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private BigDecimal participation;
+
+    public Client(ClientDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.participation = data.participation();
+    }
+}
