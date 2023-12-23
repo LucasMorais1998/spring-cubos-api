c277f55a42d99e2d5ed548a3551b7393f38cad08
Lucas Morais <lucas14.morais@gmail.com>
2023-12-23 15:39:19 -0300

    feat(client): add ClientDTO

---
+++ src/main/java/com/lucas/springcubosapi/dtos/ClientDTO.java
@@ -0,0 +1,21 @@
+package com.lucas.springcubosapi.dtos;
+
+import java.math.BigDecimal;
+
+public record ClientDTO(String firstName, String lastName, BigDecimal participation) {
+    @Override
+    public String firstName() {
+        return firstName;
+    }
+
+    @Override
+    public String lastName() {
+        return lastName;
+    }
+
+    @Override
+    public BigDecimal participation() {
+        return participation;
+    }
+
+}
