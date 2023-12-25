2999590c47ba7213e8b05ebce5066c22ae6460cd
Lucas Morais <lucas14.morais@gmail.com>
2023-12-24 23:28:49 -0300

    test(repository): add ClientRepositoryTest

---
+++ src/test/java/com/lucas/springcubosapi/repositories/ClientRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.lucas.springcubosapi.repositories;
+
+import com.lucas.springcubosapi.entities.Client;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class ClientRepositoryTest {
+    @Autowired
+    private ClientRepository clientRepository;
+
+    private Client client;
+
+    @BeforeEach
+    public void setClientConfiguration() {
+        this.client = new Client(UUID.randomUUID(), "John", "Doe", new BigDecimal(20.00));
+    }
+}
