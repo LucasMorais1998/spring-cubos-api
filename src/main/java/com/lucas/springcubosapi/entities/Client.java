9fc0e4c8b512fba3f48b85ba73a2440c01dd731e
Lucas Morais <lucas14.morais@gmail.com>
2023-12-25 01:58:28 -0300

    feat(entity): add @ToString annotation to Client

--- src/main/java/com/lucas/springcubosapi/entities/Client.java
+++ src/main/java/com/lucas/springcubosapi/entities/Client.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
+@ToString
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
